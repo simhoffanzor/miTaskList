7762d25d138e739dc9b633f04954dabddcf00b6c
simhoffanzor <simhoffanzor@gmail.com>
2022-04-03 20:07:49 -0300

    bbdd funcionando por json server

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler} from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
 
 @Injectable({
@@ -8,10 +8,14 @@ import { Task } from '../Task';
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks'
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   getTasks(): Observable<Task[]>{
-    const tasks = of(TASKS);
-    return tasks;
+    
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
