bf1c632166641d69b9a72ab445a83e2e217feca6
simhoffanzor <simhoffanzor@gmail.com>
2022-04-03 19:46:13 -0300

    creo el servicio que va a manejar la bbdd

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from '../../mock-tasks';
+import { TaskService } from '../../service/task.service';
 import { Task } from '../../Task';
-
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -10,11 +9,16 @@ import { Task } from '../../Task';
 
 export class TasksComponent implements OnInit {
 
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
   
-  constructor() { }
+  constructor(
+    private taskService: TaskService
+  ) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=>(
+      this.tasks = tasks
+    ));
   }
 
 }
